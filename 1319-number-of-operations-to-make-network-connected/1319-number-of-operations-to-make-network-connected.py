d2c9da7152f7bce0e293b2f99ec389e195fe2a89
Suzie <61377122+suzieep@users.noreply.github.com>
2023-03-24 03:32:08 +0900

    Time: 506 ms (70.02%), Space: 49.7 MB (5.26%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,22 @@
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if(len(connections) < n-1 ):
+            return -1 
+        need = 0
+        visited = [False]*(n)
+        graph = {i:[] for i in range(n)}
+        for a,b in connections:
+            graph[a].append(b)
+            graph[b].append(a)
+        for i in range(0,n):
+            if(visited[i]==0 ):
+                need += 1
+                self.dfs(graph,i,visited)
+        return need-1
+
+    def dfs(self,graph,i,visited):
+        visited[i] = True
+        for j in graph[i]:
+            if not visited[j]:
+                self.dfs(graph,j,visited);
+        return;

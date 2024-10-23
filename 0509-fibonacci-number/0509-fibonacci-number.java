4cbaa3f9b550539eae0fc439900a3f6017b02dbc
Suzie <61377122+suzieep@users.noreply.github.com>
2024-10-24 00:46:01 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (38.59%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+   public int fib(int n) {
+        int[] fib = new int[n+2];
+        fib[0]=0;
+        fib[1]=1;
+        for (int i = 2; i <= n; i++) {
+            fib[i]=fib[i-1]+fib[i-2];
+        }
+        return fib[n];
+    }
+}

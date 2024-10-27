dbd3141b201801f92d4275349a108e0c7cb5258c
Suzie <61377122+suzieep@users.noreply.github.com>
2024-10-28 03:44:55 +0900

    Time: 438 ms (9.76%), Space: 55.2 MB (34.04%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -0,0 +1,41 @@
+//2:55
+//3:28
+class Solution {
+
+    public int countSquares(int[][] matrix) {
+        int result = 0;
+        for(int i=0;i<matrix.length;i++){
+           for(int j=0;j<matrix[0].length;j++){
+               if(matrix[i][j]==1){
+                   result += 1;
+                   int side = 1;
+                   
+                   while(isSquare(i,j,matrix,side)){
+                       result +=1;
+                        System.out.println(i+" "+j+" "+side+" "+result);
+
+                       side++;
+                   }
+               }   
+            }
+        }
+        return result;
+    }
+    
+    private boolean isSquare(int i, int j, int[][] matrix, int side){
+        boolean result = true;
+        for(int k=0;k<=side;k++){
+            if(i+k<matrix.length && i+side <matrix.length
+               && j+side<matrix[0].length && j+k<matrix[0].length){
+                if(matrix[i+k][j+side] * matrix[i+side][j+k]==0){
+                    result = false;
+                }
+            } else {
+                result = false;
+            }
+        }
+                System.out.println(result);
+
+        return result;
+    }
+}
